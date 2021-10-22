2ff4c79e361ddcbe5d6a6849819a5f5c0853d864
MalliakPrasad <71307082+MalliakPrasad@users.noreply.github.com>
2021-10-22 15:39:34 +0530

    Rename analogwrite.ino to potentiometer.ino

---
+++ potentiometer.ino
@@ -0,0 +1,20 @@
+const int button=9;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(9,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int i;
+for(i=0;i<255;i=i+5)
+{
+  analogWrite(9,i);
+  delay(30);
+}
+for(i=255;i>=0;i--)
+{
+  analogWrite(9,i);
+  delay(20);
+}
+}
