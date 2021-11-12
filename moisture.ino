085471484ce5a0e7fe0a54f30fb8c4d6fa224aee
MalliakPrasad <71307082+MalliakPrasad@users.noreply.github.com>
2021-11-12 16:19:01 +0530

    Update moisture.ino

--- moisture.ino
+++ moisture.ino
@@ -1,1 +1,16 @@
+int Dpin0=5;
+int Apin0=1;
+int Apinval;
 
+void setup(){
+  pinMode(Dpin0,INPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  Serial.print("D0 Value:");
+  Serial.println(digitalRead(Dpin0));
+  Serial.print("A0 Value:");
+  Apinval=analogRead(Apinval);
+  delay(5000);
+}
